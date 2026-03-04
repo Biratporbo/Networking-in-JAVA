e673bf8aeb78db61033e913ad49e4c94a7452671
Birat <biratdey200311@gmail.com>
2026-03-04 22:14:40 +0530

    Settings up to URLConnection Class Program Details

---
+++ URL's/Details.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+
+public class Details {
+    public static void main(String[] args) throws IOException {
+        
+        //Pass the site url to URL object
+        URL obj = new URL("http://www.yahoo.com/index.html");
+
+        //open a connection with the site on Internet
+        URLConnection conn = obj.openConnection();
+
+        //display the date
+        System.out.println("Date: " + new Date(conn.getDate()));
+
+        //display the content type whether text or html
+        System.out.println("Content-type: " + conn.getContentType());
+
+        //display the expiry date
+        System.out.println("Expirey: " + conn.getExpiration());
+
+        //display the last modified date
+        System.out.println("Last Modified: " + new Date(conn.getLastModified()));
+
+        //display how many bytes the index.html page has
+        int l = conn.getContentLength();
+        System.out.println("Length of Content: " + l);
+
+        if(l == 0)
+        {
+            System.out.println("Content not available");
+            return;
+        }
+        else
+        {
+            int ch;
+            InputStream in = conn.getInputStream();
+            //display the content of the index.html page
+            while((ch = in.read())!= -1);
+            System.out.println((char)ch);
+        }
+
+    }    
+}
