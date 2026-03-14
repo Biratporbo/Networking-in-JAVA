6c140ab87ffb56c83776b39e3db9d477aa12eb6a
Birat <biratdey200311@gmail.com>
2026-03-14 22:38:04 +0530

    `Added Java code for a file server that sends a file content to a client`

--- RETRIEV_File/FileServer.java
+++ RETRIEV_File/FileServer.java
@@ -1,13 +1,3 @@
-# 🗃️ Retrieving a file at server
-
-- Let us write client and server programs, such that the client sends the name of a file to the server. After receiving the filename, the server searches for the file to know if it exists or not. If the file exists, the server sends the file contents to the client. This by following the program, which are self-explanatory.
-
----
-
-## ⌨️ Program: Write a program that accepts the filename and checks for its existence. When the file exists at server side, it sends the file contents to the client.
-
-```Java
-
 //A Server that sends a file content to the client
 
 import java.io.BufferedReader;
@@ -76,8 +66,3 @@ public class FileServer
         }
     }
 }
-
-```
-
----
-
